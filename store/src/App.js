7e57fed09382383c743fc722f638e8f16486ff4a
SamuelFlo <samuelflores100@hotmail.com>
2019-09-15 19:42:11 -0600

    ahi vamos

--- store/src/App.js
+++ store/src/App.js
@@ -1,24 +1,31 @@
 import React ,{Component}from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Default from './components/Default'
+import Product from './components/Product'
+import Details from './components/Details'
+import ProductList from './components/ProductList'
 
 class App extends Component{
   render(){
     return(
-      <div className="container">
-        <div className="row">
-          <div className="col-6">columna numero uno</div>
-          <div className="col-6">
-            <span>
-              <i className="fas fa-home"/>
-            </span>
-          </div>
-        </div>
-      </div>
+      <React.Fragment>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+
+      </React.Fragment>
 
     );
   }
 }
- 
+
 export default App;
---
+++ store/src/components/ProductList.js
@@ -0,0 +1,25 @@
+import React ,{Component}from 'react';
+import Product from './Product';
+import Title from './Title';
+import {storeProducts} from '../data';
+export default class ProductList extends Component{
+  state={
+    products: storeProducts
+  };
+  render(){
+    console.log(this.state.products);
+    return(
+      <React.Fragment>
+        <div className="py-5">
+        <div className="container">
+        <Title name="Nuestros" title="  Productos"/>
+        <div className="row"/>
+
+        </div>
+        </div>
+
+      </React.Fragment>
+
+    );
+  }
+}
