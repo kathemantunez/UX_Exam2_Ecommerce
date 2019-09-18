9511ea4bc159cc6e67c7dfb21263365542eb17aa
kathemantunez <kathe_mantunez@unitec.edu>
2019-09-17 18:41:23 -0600

    Cambio en la descripcion y fotografias de los productos

--- store/src/data.js
+++ store/src/data.js
@@ -1,113 +1,126 @@
 
-
 export const storeProducts = [
-  {
+    {
+      id: 1,
+      title: "iPhone 11 Pro Max",
+      img: "img/product-1.jpg",
+      price: 1394.10,
+      company: "Apple",
+      info:"El iPhone 11 Pro Max es una revisión del XS Max que se presentó en 2018. Como su predecesor, tiene una pantalla de 6,5 pulgadas que abarca prácticamente toda la superficie del teléfono, con  15,8 centímetros de altura por 7,78 de anchura. Cuando llegue a las tiendas el 20 de septiembre, podrá adquirirse en gris, verde, oro y plata. Una de las novedades que más ha llamado la atención tiene que ver con su cámara de triple lente con 12 MP en cada una. Además, es el primer dispositivo con iOS 13, nuevo sistema operativo que promete mejorar la rapidez, automatización de apps y la seguridad del teléfono. Para dar soporte a todos los avances tecnológicos, también tiene nuevo procesador: el A13 Bionic es capaz de llevar a cabo un billón de operaciones en un solo segundo y consigue que el iPhone Pro Max sea uno de los teléfonos más rápidos del mercado. Dependiendo de la capacidad, el coste es de 1.259 euros (64 GB), 1.429 euros (256 GB) y 1.659 euros (512 GB).",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "Samsung Galaxy Fold",
+      img: "img/product-2.jpg",
+      price: 2214.62,
+      company: "Samsung",
+      info:"Después de hacer frente a varios problemas técnicos –recordemos que su lanzamiento estaba previsto en abril y que se retrasó- parece que Samsung está preparado para sacar a la venta su Galaxy Fold, que tendrá un precio de salida de alrededor 2.000 euros.  Los dispuestos a pagar tal suman podrán hacerse con el primer teléfono plegable de la firma surcoreana, lo que le coloca a medio camino entre un móvil y una tablet. Este teléfono tiene una pantalla exterior de 4,6 pulgadas, que se complementa con la pantalla plegable de 7,3 pulgadas Infinity Flex Display. Con una memoria RAM de 12 GB, este teléfono tiene una capacidad de almacenamiento de 512 GB y, como ocurre con el Galaxy S10+, funciona con el sistema operativo Android 9 Pie. Además, se comercializará con una versión 5G y otra LTE.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "Huawei P30 Pro",
+      img: "img/product-3.jpg",
+      price: 940.11,
+      company: "Huawei",
+      info:"El 26 de marzo ha sido la fecha elegida para conocer el Huawei P30 Pro, que automáticamente da el salto a los primeros puestos del ranking de mejores móviles de 2019. Frente a la anterior generación, la de este año viene con pantalla más grande (la OLED de 6,1 pulgadas del P30 Pro crece hasta las 6,47 pulgadas), pero además añade una cuarta lente a la parte trasera. Así, el usuario se encontrará con una cámara cuádruple Leica que se compone de un objetivo gran angular de 40 MP, uno ultra gran angular de 20 MP, un teleobjetivo de 8 MP y un sensor TOF con 40 MP. En la parte frontal, tendrás una cámara de 32 MP. El Huawei P30 Pro tiene una RAM de 8 GB, pero puede adquirirse con una capacidad de almacenamiento de 128 GB, 256 GB o 512 GB. Además, este teléfono también evoluciona en sistema operativo (al pasar del Android 8 a la versión más actualizada 9 Pie) y mejora el procesador que tiene integrado (que pasa del Huawei Kirin 970 al Kirin 980).",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      title: "Samsung Galaxy Note 10+",
+      img: "img/product-4.jpg",
+      price: 1228.01,
+      company: "Samsung",
+      info:"Con una pantalla de 6,9 pulgadas (la versión estándar tiene unas nada despreciables 6,3 pulgadas), este verano llegaba el Samsung Galaxy Note 10+, que ya es compatible con el 5G. Como todos los de la familia Note, viene acompañado del ya mítico S-Pen, un boli óptico con el que el usuario puede pintar y escribir sobre la pantalla. El Galaxy Note 10+ viene con triple lente en la parte trasera (dos gran angulares de 16 y 12 MP y un teleobjetivo de 12 MP) y una delantera con 10 MP para hacer tus selfies.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      title: "LG V50",
+      img: "img/product-5.jpg",
+      price: 995.47,
+      company: "LG",
+      info:"El LGV50 es un ejemplo de lo que podemos esperar en telefonía móvil de alta gama este año: para empezar, ha sido uno de los dispositivos del  MWC compatible con la tecnología 5G. Además, a su pantalla de 6,4 pulgadas puedes incorporarle una segunda pantalla externa, que se conecta a la principal como un accesorio, lo que no deja de ser una manera de apuntarse a la tendencia de las superficies flexibles de teléfonos como el Galaxy Fold. Este teléfono incorpora un procesador Snapdragon 855, una memoria RAM de 6GB y una capacidad de almacenamiento de 128 GB. Además, cuenta con una cámara trasera de triple lente con 16 MP, y dos con 12 MP, además de una cámara frontal con 8 MP.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      title: "Xiaomi Mi 9",
+      img: "img/product-6.png",
+      price: 663.28,
+      company: "Xiaomi",
+      info:"El Xiaomi Mi 9, con un precio que no llega a los 500 euros en su versión más básica, es una opción interesante si no quieres gastar un dineral en renovar tu teléfono, pero tampoco quieres renunciar a la alta gama. El último dispositivo de la firma china tiene una pantalla de 6,93 pulgadas y su diseño, como el del Mi 8, evoca al de los últimos iPhone, otros de los mejores móviles de nuestro ranking. En cuanto a la cámara, cuenta con una de triple lente en la parte trasera, que incorpora tres sensores de 48 MP, 12 MP y 16 MP. En la parte delantera, para que puedas hacer buenos selfies, la cámara es de 20 MP. Funciona con un procesador Snapdragon 855 y Android 9.0, y podrá adquirirse con una memoria de 6 GB o de 8 GB de RAM y unos 128 o 256 GB de memoria interna.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      title: "LG G8 ThinQ",
+      img: "img/product-7.jpg",
+      price: 884.74,
+      company: "LG",
+      info:"Uno de los mejores móviles presentados en lo que llevamos de año es el LG G8 ThinQ, un teléfono que, como su predecesor, presenta marcos casi inexistentes y una pantalla LCD QHD+ de 6,1 pulgadas. Este teléfono es uno de los primeros de la firma en estrenar el procesador Snapdragon 855. En el LG G8 encontrarás una triple cámara trasera con 16 MP el objetivo principal y 12 MP en los otros dos y, para tus selfies, una frontal de 8 MP que permite enfocar hasta tres personas y darle efecto bokeh (con el fondo desenfocado) a tus capturas.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 8,
+      title: "Samsung Galaxy S10+",
+      img: "img/product-8.jpg",
+      price: 995.47,
+      company: "Samsung",
+      info:"Aunque su presentación en el MWC 2019 no era ninguna sorpresa, una de las novedades de esta edición ha sido el Samsung Galaxy S10+, que ha llegado acompañado de su versión Galaxy S10+ Plus, el que será uno de los mejores móviles del mercado en 2019. A simple vista, este modelo no presenta demasiadas diferencias frente al anterior de la gama, pero innova en calidad fotográfica, al incorporar una cámara trasera triple con 16 MP, 12 MP y 12 MP. En el frontal, encontrarás una lente dual con 10 y 8 MP. Con un procesador Exynos 9820 integrado y una memoria RAM de 8 GB y hasta 512 GB de almacenamiento, una de las novedades de este teléfono es que será el primero del fabricante surcoreano que incorporará Android 9 Pie.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+        id: 9,
+        title: "Google Pixel 3 XL",
+        img: "img/product-9.jpg",
+        price: 940.11,
+        company: "Google",
+        info:"El Google Pixel 3 XL es el teléfono más grande de la compañía, con una pantalla de 6,3 pulgadas que aprovecha casi toda la superficie del teléfono y un lector de huellas digitales en la parte de atrás. Otro de los atractivos del teléfono es la cámara trasera con doble lente de 12 MP y la frontal de 8 MP. Por lo demás, tiene una memoria RAM de 4 GB y el usuario puede elegir entre una capacidad de almacenamiento de 64 GB o 128 GB. Además, está disponible en tres colores: un rosa pálido, blanco y negro.",
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id: 10,
+        title: "ZTE One Plus 7 Pro",
+        img: "img/product-10.jpg",
+        price: 786.19,
+        company: "ZTE",
+        info:"Presentado junto al ZTE One Plus 7 llegaba en mayo la versión Pro de este teléfono, con un salto en calidad frente a los anteriores modelos de la firma china. Con 6,67 pulgadas de pantalla AMOLED y marcos apurados al límite, este teléfono es uno de los más interesantes de nuestra lista si nos fijamos en el precio, con un coste más asequible que el de otros modelos de la marca. Eso no implica que esté bien equipado: cuenta con lector facial y de huella dactilar y, como la cámara es una de las cosas que más importan a día de hoy, no decepciona en este sentido: trae una dual de 48 MP y 8 MP situada en la parte trasera y una en el frontal con 16 MP para tus selfies. Además, el ZTE One Plus 7 Pro viene con una memoria RAM de 8 GB, un procesador Snapdragon 855 y el sistema operativo Android 9 Pie.",
+        inCart: false,
+        count: 0,
+        total: 0
+      }
+  ];
+  
+  export const detailProduct = {
     id: 1,
-    title: "Google Pixel - Black",
-    img: "img/product-1.png",
-    price: 10,
-    company: "GOOGLE",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    title: "iPhone 11 Pro Max",
+    img: "img/product-1.jpg",
+    price: 1394.10,
+    company: "Apple",
+    info:"El iPhone 11 Pro Max es una revisión del XS Max que se presentó en 2018. Como su predecesor, tiene una pantalla de 6,5 pulgadas que abarca prácticamente toda la superficie del teléfono, con  15,8 centímetros de altura por 7,78 de anchura. Cuando llegue a las tiendas el 20 de septiembre, podrá adquirirse en gris, verde, oro y plata. Una de las novedades que más ha llamado la atención tiene que ver con su cámara de triple lente con 12 MP en cada una. Además, es el primer dispositivo con iOS 13, nuevo sistema operativo que promete mejorar la rapidez, automatización de apps y la seguridad del teléfono. Para dar soporte a todos los avances tecnológicos, también tiene nuevo procesador: el A13 Bionic es capaz de llevar a cabo un billón de operaciones en un solo segundo y consigue que el iPhone Pro Max sea uno de los teléfonos más rápidos del mercado. Dependiendo de la capacidad, el coste es de 1.259 euros (64 GB), 1.429 euros (256 GB) y 1.659 euros (512 GB).",
     inCart: false,
     count: 0,
     total: 0
-  },
-  {
-    id: 2,
-    title: "Samsung S7",
-    img: "img/product-2.png",
-    price: 16,
-    company: "SAMSUNG",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 3,
-    title: "HTC 10 - Black",
-    img: "img/product-3.png",
-    price: 8,
-    company: "htc",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 4,
-    title: "HTC 10 - White",
-    img: "img/product-4.png",
-    price: 18,
-    company: "htc",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 5,
-    title: "HTC Desire 626s",
-    img: "img/product-5.png",
-    price: 24,
-    company: "htc",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 6,
-    title: "Vintage Iphone",
-    img: "img/product-6.png",
-    price: 17,
-    company: "apple",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 7,
-    title: "Iphone 7",
-    img: "img/product-7.png",
-    price: 30,
-    company: "apple",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 8,
-    title: "Smashed Iphone",
-    img: "img/product-8.png",
-    price: 2,
-    company: "apple",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  }
-];
-
-export const detailProduct = {
-  id: 1,
-  title: "Google Pixel - Black",
-  img: "img/product-1.png",
-  price: 10,
-  company: "google",
-  info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-  inCart: false,
-  count: 0,
-  total: 0
-};
+  };
+  
